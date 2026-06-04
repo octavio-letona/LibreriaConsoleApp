fa16cbcc6f68ef31a17305aa0ed2bb7189d7fc23
octavio letona <oletona-2026097@kinal.edu.gt>
2026-06-04 11:31:11 -0600

    000 - Primer commit. Configuracion basica y .gitignore

---
+++ src/org/octavioletona/system/Principal.java
@@ -0,0 +1,13 @@
+
+package org.octavioletona.system;
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo ");
+    }
+    
+}
