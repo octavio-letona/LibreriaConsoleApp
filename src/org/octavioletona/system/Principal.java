1b0ff795e519bf485901e5d116f7242a3ab7ff47
Informatica <informatica@alumno.local>
2026-06-05 10:47:24 -0600

    006 Se actualizo la clase Principal

--- src/org/octavioletona/system/Principal.java
+++ src/org/octavioletona/system/Principal.java
@@ -7,7 +7,8 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo ");
+        System.out.println("hola mundo - Soy Juan Carlos ");
+        Systen¿m.out.println("Estoy en kinal - 2026061");
     }
     
 }
