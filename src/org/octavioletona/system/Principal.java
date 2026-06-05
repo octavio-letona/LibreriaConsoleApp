4a28310449f92a6077d9180024eea519c195af44
Informatica <informatica@alumno.local>
2026-06-05 10:45:05 -0600

    010 se actualizo la clase principal y agrege un saludo

--- src/org/octavioletona/system/Principal.java
+++ src/org/octavioletona/system/Principal.java
@@ -7,7 +7,8 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo ");
+        System.out.println("Hola mundo soy el octavio");
+        System.out.println("Estoy en kinal - 2026097");
     }
     
 }
