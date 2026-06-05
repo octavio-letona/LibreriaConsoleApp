6dad235054a561939122b1226e9305490737f24c
Informatica <informatica@alumno.local>
2026-06-05 10:48:41 -0600

    006 se actualoso la pagina principa

--- src/org/octavioletona/system/Principal.java
+++ src/org/octavioletona/system/Principal.java
@@ -7,7 +7,8 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo ");
+        System.out.println("hola mundo soy cristofer  ");
+           System.out.println("estoy en kinal -2026120  ");
     }
     
 }
